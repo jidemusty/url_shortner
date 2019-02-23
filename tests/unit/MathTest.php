f1bdfc8b86e320330c64efb2880a18466b5b95f0
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2019-02-22 19:51:32 -0600

    shortcode generation

---
+++ tests/unit/MathTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class MathTest extends TestCase
+{
+    protected $mappings = [
+        1 => 1,
+        100 => '1C',
+        1000000 => '4c92'
+    ];
+
+    /** @test */
+    public function correctly_encodes_values()
+    {
+        $math = new \App\Helpers\Math;
+
+        foreach ($this->mappings as $value => $encoded) {
+            $this->assertEquals($encoded, $math->toBase($value));
+        }
+    }
+}
